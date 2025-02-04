c54684aa36266186e143d56cd4c85542c057df6e
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-04 13:09:16 -0300

    add: utils and types

---
+++ types/auction.ts
@@ -0,0 +1,10 @@
+export interface Auction {
+  id: number;
+  title: string;
+  image: string;
+  currentBid: number;
+  endTime: string;
+  location: string;
+  latitude: number;
+  longitude: number;
+}
