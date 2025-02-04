df960eb8a47ef9ce0a4fd6fd6fa612a0f58154c4
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-04 13:06:31 -0300

    project config

---
+++ app/data/mockAuctionData.ts
@@ -0,0 +1,136 @@
+import { Auction } from "@/types/auction";
+
+export const auctions: Auction[] = [
+  {
+    id: 1,
+    title: "Carro Antigo",
+    currentBid: 10000,
+    endTime: "2023-07-01",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -23.4551841,
+    longitude: -46.8807891,
+    location: "São Paulo",
+  },
+  {
+    id: 2,
+    title: "Obra de Arte",
+    currentBid: 5000,
+    endTime: "2023-06-30",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -22.9068,
+    longitude: -43.1729,
+    location: "Rio de Janeiro",
+  },
+  {
+    id: 3,
+    title: "Imóvel",
+    currentBid: 200000,
+    endTime: "2023-07-15",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -19.9167,
+    longitude: -43.9345,
+    location: "Belo Horizonte",
+  },
+  {
+    id: 4,
+    title: "Carro Antigo",
+    currentBid: 10000,
+    endTime: "2023-07-01",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -23.5505,
+    longitude: -46.6333,
+    location: "São Paulo",
+  },
+  {
+    id: 5,
+    title: "Obra de Arte",
+    currentBid: 5000,
+    endTime: "2023-06-30",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -22.9068,
+    longitude: -43.1729,
+    location: "Rio de Janeiro",
+  },
+  {
+    id: 6,
+    title: "Imóvel",
+    currentBid: 200000,
+    endTime: "2023-07-15",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -19.9167,
+    longitude: -43.9345,
+    location: "Belo Horizonte",
+  },
+  {
+    id: 7,
+    title: "Carro Antigo",
+    currentBid: 10000,
+    endTime: "2023-07-01",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -23.5505,
+    longitude: -46.6333,
+    location: "São Paulo",
+  },
+  {
+    id: 8,
+    title: "Obra de Arte",
+    currentBid: 5000,
+    endTime: "2023-06-30",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -22.9068,
+    longitude: -43.1729,
+    location: "Rio de Janeiro",
+  },
+  {
+    id: 9,
+    title: "Imóvel",
+    currentBid: 200000,
+    endTime: "2023-07-15",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -19.9167,
+    longitude: -43.9345,
+    location: "Belo Horizonte",
+  },
+  {
+    id: 10,
+    title: "Carro Antigo",
+    currentBid: 10000,
+    endTime: "2023-07-01",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -23.5505,
+    longitude: -46.6333,
+    location: "São Paulo",
+  },
+  {
+    id: 11,
+    title: "Obra de Arte",
+    currentBid: 5000,
+    endTime: "2023-06-30",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -22.9068,
+    longitude: -43.1729,
+    location: "Rio de Janeiro",
+  },
+  {
+    id: 12,
+    title: "Imóvel",
+    currentBid: 200000,
+    endTime: "2023-07-15",
+    image:
+      "https://cdn.motor1.com/images/mgl/VobQz/s1/10-carros-brasileiros-com-nomes-curiosos-no-exterior.webp",
+    latitude: -19.9167,
+    longitude: -43.9345,
+    location: "Belo Horizonte",
+  },
+];
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: ["cdn.motor1.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.motor1.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
