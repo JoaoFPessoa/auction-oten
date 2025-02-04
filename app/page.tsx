d44caecbcc246b2fedb4ecdcb0bf9a241dcde203
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-04 16:18:12 -0300

    removed map to test window error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AuctionList from "@/components/auction/auction-list";
 import SearchFilter from "@/components/search-filter";
 import { auctions } from "../data/mockAuctionData";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const AuctionMap = dynamic(() => import("@/components/auction/auction-map"));
+// const AuctionMap = dynamic(() => import("@/components/auction/auction-map"));
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
       <AuctionList auctions={auctions} />
       <div className="max-w-7xl mx-auto h-[800px] relative mt-12 mb-4">
         <h2 className="text-2xl font-bold">Mapa de Leilões</h2>
-        <AuctionMap auctions={auctions} />
+        {/* <AuctionMap auctions={auctions} /> */}
       </div>
     </main>
   );
