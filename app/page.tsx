96eb3b019d6aac6450e8061b5e993272ef0083ef
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-04 16:04:53 -0300

    removed unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { Suspense } from "react";
 import AuctionList from "@/components/auction/auction-list";
 import SearchFilter from "@/components/search-filter";
-import { Skeleton } from "@/components/ui/skeleton";
 import { auctions } from "../data/mockAuctionData";
 import AuctionMap from "@/components/auction/auction-map";
 
@@ -9,9 +7,7 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <SearchFilter />
-      <Suspense fallback={<Skeleton className="w-full h-96" />}>
-        <AuctionList auctions={auctions} />
-      </Suspense>
+      <AuctionList auctions={auctions} />
       <div className="max-w-7xl mx-auto h-[800px] relative mt-12 mb-4">
         <h2 className="text-2xl font-bold">Mapa de Leilões</h2>
         <AuctionMap auctions={auctions} />
