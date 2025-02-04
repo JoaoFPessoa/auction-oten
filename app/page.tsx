0df53e3b68d27843e21aebc1fa7e1467b5cca7b3
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-04 16:16:09 -0300

    add dynamic import on auction map

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import AuctionList from "@/components/auction/auction-list";
 import SearchFilter from "@/components/search-filter";
+import { auctions } from "../data/mockAuctionData";
+import dynamic from "next/dynamic";
+
+const AuctionMap = dynamic(() => import("@/components/auction/auction-map"));
 
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <SearchFilter />
-      {/* <AuctionList auctions={auctions} /> */}
+      <AuctionList auctions={auctions} />
       <div className="max-w-7xl mx-auto h-[800px] relative mt-12 mb-4">
         <h2 className="text-2xl font-bold">Mapa de Leilões</h2>
-        {/* <AuctionMap auctions={auctions} /> */}
+        <AuctionMap auctions={auctions} />
       </div>
     </main>
   );
