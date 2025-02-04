cf9470594f64b6ce3fca32cd80b38537172c92f4
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-04 14:44:57 -0300

    removed auction map

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from "react";
 import AuctionList from "@/components/auction/auction-list";
 import SearchFilter from "@/components/search-filter";
 import { Skeleton } from "@/components/ui/skeleton";
-import AuctionMap from "@/components/auction/auction-map";
 import { auctions } from "../data/mockAuctionData";
 
 export default function Home() {
@@ -12,12 +11,12 @@ export default function Home() {
       <Suspense fallback={<Skeleton className="w-full h-96" />}>
         <AuctionList auctions={auctions} />
       </Suspense>
-      <Suspense>
+      {/* <Suspense>
         <h2 className="text-2xl max-w-7xl mx-auto font-bold mt-12 mb-4">
           Mapa de Leilões
           <AuctionMap auctions={auctions} />
         </h2>
-      </Suspense>
+      </Suspense> */}
     </main>
   );
 }
